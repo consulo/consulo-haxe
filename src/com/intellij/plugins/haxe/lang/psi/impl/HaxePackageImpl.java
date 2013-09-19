1836f2eeb6c791090551e27ae5900c37f0dbdf08
VISTALL <vistall.valeriy@gmail.com>
2013-09-19 07:03:46 +0300

    dead method

--- src/com/intellij/plugins/haxe/lang/psi/impl/HaxePackageImpl.java
+++ src/com/intellij/plugins/haxe/lang/psi/impl/HaxePackageImpl.java
@@ -3,7 +3,6 @@ package com.intellij.plugins.haxe.lang.psi.impl;
 import com.intellij.lang.Language;
 import com.intellij.plugins.haxe.HaxeLanguage;
 import com.intellij.plugins.haxe.lang.psi.HaxePackage;
-import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.file.PsiPackageBase;
 import com.intellij.util.ArrayFactory;
@@ -12,9 +11,6 @@ import org.consulo.psi.PsiPackage;
 import org.consulo.psi.PsiPackageManager;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.Collections;
-
 /**
  * @author VISTALL
  * @since 12.09.13.
@@ -22,11 +18,6 @@ import java.util.Collections;
 public class HaxePackageImpl extends PsiPackageBase implements HaxePackage {
 	public HaxePackageImpl(PsiManager manager, PsiPackageManager packageManager, Class<? extends ModuleExtension> extensionClass, String qualifiedName) {
 		super(manager, packageManager, extensionClass, qualifiedName);
-	}
-
-	@Override
-	protected Collection<PsiDirectory> getAllDirectories() {
-		return Collections.emptyList();
 	}
 
 	@Override
