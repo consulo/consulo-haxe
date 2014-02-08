0bf052cf1a7ef7d716cfaff2a434221005d2b886
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 23:32:22 +0200

    compilation fixed

--- src/com/intellij/plugins/haxe/ide/structure/HaxeStructureViewFactory.java
+++ src/com/intellij/plugins/haxe/ide/structure/HaxeStructureViewFactory.java
@@ -15,29 +15,37 @@
  */
 package com.intellij.plugins.haxe.ide.structure;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
-public class HaxeStructureViewFactory implements PsiStructureViewFactory {
-  @Override
-  public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-    return new TreeBasedStructureViewBuilder() {
-      @NotNull
-      public StructureViewModel createStructureViewModel() {
-        return new HaxeStructureViewModel(psiFile);
-      }
+public class HaxeStructureViewFactory implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@Override
+			@NotNull
+			public StructureViewModel createStructureViewModel(@Nullable Editor editor)
+			{
+				return new HaxeStructureViewModel(psiFile);
+			}
 
-      @Override
-      public boolean isRootNodeShown() {
-        return false;
-      }
-    };
-  }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
