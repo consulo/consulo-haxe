a8198fcda928c855a5c93de9d101a683c4aeaf5f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:25:09 +0400

    drop usage EP_NAMe

--- src/main/java/com/intellij/plugins/haxe/config/sdk/HaxeSdkType.java
+++ src/main/java/com/intellij/plugins/haxe/config/sdk/HaxeSdkType.java
@@ -16,7 +16,9 @@
 package com.intellij.plugins.haxe.config.sdk;
 
 import com.intellij.plugins.haxe.HaxeBundle;
+import com.intellij.plugins.haxe.HaxeIcons;
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.application.util.SystemInfo;
 import consulo.content.OrderRootType;
 import consulo.content.base.BinariesOrderRootType;
@@ -27,117 +29,116 @@ import consulo.ui.image.Image;
 import consulo.util.xml.serializer.XmlSerializer;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.VirtualFileManager;
-import com.intellij.plugins.haxe.HaxeIcons;
+import jakarta.annotation.Nonnull;
 import org.jdom.Element;
 
-import jakarta.annotation.Nonnull;
 import java.util.Collection;
 import java.util.Collections;
 
 @ExtensionImpl
 public class HaxeSdkType extends SdkType {
-  @Nonnull
-  public static HaxeSdkType getInstance() {
-    return EP_NAME.findExtensionOrFail(HaxeSdkType.class);
-  }
-
-  public HaxeSdkType() {
-    super("HAXE_SDK");
-  }
-
-  @Override
-  public Image getIcon() {
-    return HaxeIcons.Haxe;
-  }
-
-  @Nonnull
-  @Override
-  public String getPresentableName() {
-    return HaxeBundle.message("haxe.sdk.name.presentable");
-  }
-
-  @Override
-  public String suggestSdkName(String currentSdkName, String sdkHome) {
-    return HaxeBundle.message("haxe.sdk.name.suggest", getVersionString(sdkHome));
-  }
-
-  @Override
-  public String getVersionString(String sdkHome) {
-    final HaxeSdkData haxeSdkData = HaxeSdkUtil.testHaxeSdk(sdkHome);
-    return haxeSdkData != null ? haxeSdkData.getVersion() : null;
-  }
-
-  @Nonnull
-  @Override
-  public Collection<String> suggestHomePaths() {
-    String result = System.getenv("HAXEPATH");
-    if (result == null && !SystemInfo.isWindows) {
-      final String candidate = "/usr/lib/haxe";
-      if (VirtualFileManager.getInstance().findFileByUrl(candidate) != null) {
-        result = candidate;
-      }
-    }
-    if (result != null) {
-      return Collections.singletonList(result);
-    }
-    return Collections.emptyList();
-  }
-
-  @Override
-  public boolean canCreatePredefinedSdks() {
-    return true;
-  }
-
-  @Override
-  public boolean isValidSdkHome(String path) {
-    return HaxeSdkUtil.testHaxeSdk(path) != null;
-  }
-
-  @Override
-  public AdditionalDataConfigurable createAdditionalDataConfigurable(SdkModel sdkModel, SdkModificator sdkModificator) {
-    return new HaxeAdditionalConfigurable();
-  }
-
-  @Override
-  public boolean isRootTypeApplicable(OrderRootType type) {
-    return type == SourcesOrderRootType.getInstance() || type == BinariesOrderRootType.getInstance() || type == DocumentationOrderRootType.getInstance();
-  }
-
-  @Override
-  public void setupSdkPaths(Sdk sdk) {
-    final SdkModificator modificator = sdk.getSdkModificator();
-
-    SdkAdditionalData data = sdk.getSdkAdditionalData();
-    if (data == null) {
-      data = HaxeSdkUtil.testHaxeSdk(sdk.getHomePath());
-      modificator.setSdkAdditionalData(data);
+    @Nonnull
+    public static HaxeSdkType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(HaxeSdkType.class);
     }
 
-    VirtualFile homeDirectory = sdk.getHomeDirectory();
-    if (homeDirectory != null) {
-      final VirtualFile stdRoot = homeDirectory.findChild("std");
-      if (stdRoot != null) {
-        modificator.addRoot(stdRoot, BinariesOrderRootType.getInstance());
-        modificator.addRoot(stdRoot, SourcesOrderRootType.getInstance());
-      }
-      final VirtualFile docRoot = homeDirectory.findChild("doc");
-      if (docRoot != null) {
-        modificator.addRoot(docRoot, DocumentationOrderRootType.getInstance());
-      }
+    public HaxeSdkType() {
+        super("HAXE_SDK");
     }
 
-    modificator.commitChanges();
-  }
-
-  @Override
-  public SdkAdditionalData loadAdditionalData(Sdk sdk, Element additional) {
-    return XmlSerializer.deserialize(additional, HaxeSdkData.class);
-  }
-
-  @Override
-  public void saveAdditionalData(SdkAdditionalData additionalData, Element additional) {
-    if (additionalData instanceof HaxeSdkData) {
-      XmlSerializer.serializeInto(additionalData, additional);
+    @Override
+    public Image getIcon() {
+        return HaxeIcons.Haxe;
     }
-  }
+
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return HaxeBundle.message("haxe.sdk.name.presentable");
+    }
+
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return HaxeBundle.message("haxe.sdk.name.suggest", getVersionString(sdkHome));
+    }
+
+    @Override
+    public String getVersionString(String sdkHome) {
+        final HaxeSdkData haxeSdkData = HaxeSdkUtil.testHaxeSdk(sdkHome);
+        return haxeSdkData != null ? haxeSdkData.getVersion() : null;
+    }
+
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        String result = System.getenv("HAXEPATH");
+        if (result == null && !SystemInfo.isWindows) {
+            final String candidate = "/usr/lib/haxe";
+            if (VirtualFileManager.getInstance().findFileByUrl(candidate) != null) {
+                result = candidate;
+            }
+        }
+        if (result != null) {
+            return Collections.singletonList(result);
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean canCreatePredefinedSdks() {
+        return true;
+    }
+
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return HaxeSdkUtil.testHaxeSdk(path) != null;
+    }
+
+    @Override
+    public AdditionalDataConfigurable createAdditionalDataConfigurable(SdkModel sdkModel, SdkModificator sdkModificator) {
+        return new HaxeAdditionalConfigurable();
+    }
+
+    @Override
+    public boolean isRootTypeApplicable(OrderRootType type) {
+        return type == SourcesOrderRootType.getInstance() || type == BinariesOrderRootType.getInstance() || type == DocumentationOrderRootType.getInstance();
+    }
+
+    @Override
+    public void setupSdkPaths(Sdk sdk) {
+        final SdkModificator modificator = sdk.getSdkModificator();
+
+        SdkAdditionalData data = sdk.getSdkAdditionalData();
+        if (data == null) {
+            data = HaxeSdkUtil.testHaxeSdk(sdk.getHomePath());
+            modificator.setSdkAdditionalData(data);
+        }
+
+        VirtualFile homeDirectory = sdk.getHomeDirectory();
+        if (homeDirectory != null) {
+            final VirtualFile stdRoot = homeDirectory.findChild("std");
+            if (stdRoot != null) {
+                modificator.addRoot(stdRoot, BinariesOrderRootType.getInstance());
+                modificator.addRoot(stdRoot, SourcesOrderRootType.getInstance());
+            }
+            final VirtualFile docRoot = homeDirectory.findChild("doc");
+            if (docRoot != null) {
+                modificator.addRoot(docRoot, DocumentationOrderRootType.getInstance());
+            }
+        }
+
+        modificator.commitChanges();
+    }
+
+    @Override
+    public SdkAdditionalData loadAdditionalData(Sdk sdk, Element additional) {
+        return XmlSerializer.deserialize(additional, HaxeSdkData.class);
+    }
+
+    @Override
+    public void saveAdditionalData(SdkAdditionalData additionalData, Element additional) {
+        if (additionalData instanceof HaxeSdkData) {
+            XmlSerializer.serializeInto(additionalData, additional);
+        }
+    }
 }
