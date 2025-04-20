25ed26cb834ae0e94e181388ba53c55fe8f5c594
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:51:40 +0400

    drop usage EP_NAME

--- src/main/java/com/intellij/plugins/haxe/ide/library/HaxeLibraryType.java
+++ src/main/java/com/intellij/plugins/haxe/ide/library/HaxeLibraryType.java
@@ -15,7 +15,9 @@
  */
 package com.intellij.plugins.haxe.ide.library;
 
+import com.intellij.plugins.haxe.HaxeIcons;
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.content.library.*;
 import consulo.content.library.ui.LibraryEditorComponent;
 import consulo.content.library.ui.LibraryPropertiesEditor;
@@ -23,62 +25,60 @@ import consulo.content.library.ui.LibraryRootsComponentDescriptor;
 import consulo.project.Project;
 import consulo.ui.image.Image;
 import consulo.virtualFileSystem.VirtualFile;
-import com.intellij.plugins.haxe.HaxeIcons;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import javax.swing.*;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
 @ExtensionImpl
 public class HaxeLibraryType extends LibraryType<DummyLibraryProperties> {
-  public static final PersistentLibraryKind<DummyLibraryProperties> HAXE_LIBRARY =
-      new PersistentLibraryKind<DummyLibraryProperties>("haXe") {
+    public static final PersistentLibraryKind<DummyLibraryProperties> HAXE_LIBRARY = new PersistentLibraryKind<>("haXe") {
         @Nonnull
         @Override
         public DummyLibraryProperties createDefaultProperties() {
-          return new DummyLibraryProperties();
+            return new DummyLibraryProperties();
         }
-      };
+    };
 
-  public HaxeLibraryType() {
-    super(HAXE_LIBRARY);
-  }
+    public HaxeLibraryType() {
+        super(HAXE_LIBRARY);
+    }
 
-  @Nonnull
-  @Override
-  public String getCreateActionName() {
-    return "haXe";
-  }
+    @Nonnull
+    @Override
+    public String getCreateActionName() {
+        return "haXe";
+    }
 
-  @Override
-  public NewLibraryConfiguration createNewLibrary(@Nonnull JComponent parentComponent,
-                                                  @Nullable VirtualFile contextDirectory,
-                                                  @Nonnull Project project) {
+    @Override
+    public NewLibraryConfiguration createNewLibrary(@Nonnull JComponent parentComponent,
+                                                    @Nullable VirtualFile contextDirectory,
+                                                    @Nonnull Project project) {
 
-    return LibraryTypeService.getInstance()
-        .createLibraryFromFiles(createLibraryRootsComponentDescriptor(), parentComponent, contextDirectory, this, project);
-  }
+        return LibraryTypeService.getInstance()
+            .createLibraryFromFiles(createLibraryRootsComponentDescriptor(), parentComponent, contextDirectory, this, project);
+    }
 
-  @Nonnull
-  @Override
-  public LibraryRootsComponentDescriptor createLibraryRootsComponentDescriptor() {
-    return new HaxeLibraryRootsComponentDescriptor();
-  }
+    @Nonnull
+    @Override
+    public LibraryRootsComponentDescriptor createLibraryRootsComponentDescriptor() {
+        return new HaxeLibraryRootsComponentDescriptor();
+    }
 
-  @Override
-  public LibraryPropertiesEditor createPropertiesEditor(@Nonnull LibraryEditorComponent<DummyLibraryProperties> component) {
-    return null;
-  }
+    @Override
+    public LibraryPropertiesEditor createPropertiesEditor(@Nonnull LibraryEditorComponent<DummyLibraryProperties> component) {
+        return null;
+    }
 
-  @Override
-  public Image getIcon() {
-    return HaxeIcons.Haxe;
-  }
+    @Override
+    public Image getIcon() {
+        return HaxeIcons.Haxe;
+    }
 
-  public static HaxeLibraryType getInstance() {
-    return LibraryType.EP_NAME.findExtension(HaxeLibraryType.class);
-  }
+    public static HaxeLibraryType getInstance() {
+        return Application.get().getExtensionPoint(LibraryType.class).findExtension(HaxeLibraryType.class);
+    }
 }
