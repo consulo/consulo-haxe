a97dab6ec802e3c6bb140865a536ef2e7f7a274b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-07-04 01:13:14 +0300

    migrate to new action api

--- src/main/java/com/intellij/plugins/haxe/ide/generation/BaseHaxeGenerateAction.java
+++ src/main/java/com/intellij/plugins/haxe/ide/generation/BaseHaxeGenerateAction.java
@@ -15,49 +15,52 @@
  */
 package com.intellij.plugins.haxe.ide.generation;
 
+import com.intellij.plugins.haxe.lang.psi.HaxeClass;
+import consulo.codeEditor.Editor;
 import consulo.language.editor.LangDataKeys;
 import consulo.language.editor.PlatformDataKeys;
-import consulo.language.psi.util.PsiTreeUtil;
-import consulo.ui.ex.action.AnActionEvent;
-import consulo.codeEditor.Editor;
-import consulo.project.Project;
-import consulo.util.lang.Pair;
-import com.intellij.plugins.haxe.lang.psi.HaxeClass;
 import consulo.language.psi.PsiFile;
+import consulo.language.psi.util.PsiTreeUtil;
+import consulo.project.Project;
 import consulo.ui.ex.action.AnAction;
+import consulo.ui.ex.action.AnActionEvent;
+import consulo.ui.ex.action.AnActionWithSyncUpdate;
+import consulo.util.lang.Pair;
 
 /**
- * @author: Fedor.Korotkov
+ * @author Fedor.Korotkov
  */
-public abstract class BaseHaxeGenerateAction extends AnAction {
+public abstract class BaseHaxeGenerateAction extends AnAction implements AnActionWithSyncUpdate {
 
-  public void actionPerformed(final AnActionEvent e) {
-    final Project project = e.getData(PlatformDataKeys.PROJECT);
-    assert project != null;
-    final Pair<Editor, PsiFile> editorAndPsiFile = getEditorAndPsiFile(e);
-    getGenerateHandler().invoke(project, editorAndPsiFile.first, editorAndPsiFile.second);
-  }
+    public void actionPerformed(final AnActionEvent e) {
+        final Project project = e.getData(PlatformDataKeys.PROJECT);
+        assert project != null;
+        final Pair<Editor, PsiFile> editorAndPsiFile = getEditorAndPsiFile(e);
+        getGenerateHandler().invoke(project, editorAndPsiFile.first, editorAndPsiFile.second);
+    }
 
-  private static Pair<Editor, PsiFile> getEditorAndPsiFile(final AnActionEvent e) {
-    final Project project = e.getData(PlatformDataKeys.PROJECT);
-    if (project == null) return Pair.create(null, null);
-    Editor editor = e.getData(PlatformDataKeys.EDITOR);
-    PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-    return Pair.create(editor, psiFile);
-  }
+    private static Pair<Editor, PsiFile> getEditorAndPsiFile(final AnActionEvent e) {
+        final Project project = e.getData(PlatformDataKeys.PROJECT);
+        if (project == null) {
+            return Pair.create(null, null);
+        }
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        return Pair.create(editor, psiFile);
+    }
 
-  protected abstract BaseHaxeGenerateHandler getGenerateHandler();
+    protected abstract BaseHaxeGenerateHandler getGenerateHandler();
 
-  @Override
-  public void update(final AnActionEvent e) {
-    final Pair<Editor, PsiFile> editorAndPsiFile = getEditorAndPsiFile(e);
-    final Editor editor = editorAndPsiFile.first;
-    final PsiFile psiFile = editorAndPsiFile.second;
+    @Override
+    public void update(final AnActionEvent e) {
+        final Pair<Editor, PsiFile> editorAndPsiFile = getEditorAndPsiFile(e);
+        final Editor editor = editorAndPsiFile.first;
+        final PsiFile psiFile = editorAndPsiFile.second;
 
-    final int caretOffset = editor == null ? -1 : editor.getCaretModel().getOffset();
-    final boolean inClass = psiFile != null && PsiTreeUtil.getParentOfType(psiFile.findElementAt(caretOffset), HaxeClass.class) != null;
+        final int caretOffset = editor == null ? -1 : editor.getCaretModel().getOffset();
+        final boolean inClass = psiFile != null && PsiTreeUtil.getParentOfType(psiFile.findElementAt(caretOffset), HaxeClass.class) != null;
 
-    e.getPresentation().setEnabled(inClass);
-    e.getPresentation().setVisible(inClass);
-  }
+        e.getPresentation().setEnabled(inClass);
+        e.getPresentation().setVisible(inClass);
+    }
 }
